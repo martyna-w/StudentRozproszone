c89ad0eb112ee359d990743bd401446f6968fd7f
michalKoczewski <52933683+michalKoczewski@users.noreply.github.com>
2020-06-04 00:12:54 +0200

    3

--- src/app/add-question/add-question.component.ts
+++ src/app/add-question/add-question.component.ts
@@ -20,19 +20,17 @@ export class AddQuestionComponent implements OnInit {
   fakeAnwserThree: string;
   result: string;
 
-  constructor(private qs: QuestionsService, private formBuilder: FormBuilder) { }
+  constructor(private qs: QuestionsService, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.AddQuestionForm = this.formBuilder.group({
-      NowePytanie: ['', [Validators.required, Validators.minLength(10)]],
-      NowaDobraOdp: ['', [Validators.required, Validators.minLength(10)]],
-      Nowa1ZlaOdp: ['', [Validators.required, Validators.minLength(10)]],
-      Nowa2ZlaOdp: ['', [Validators.required, Validators.minLength(10)]],
-      Nowa3ZlaOdp: ['', [Validators.required, Validators.minLength(10)]],
-    })
+      NowePytanie: ["", [Validators.required, Validators.minLength(10)]],
+      NowaDobraOdp: ["", [Validators.required, Validators.minLength(10)]],
+      Nowa1ZlaOdp: ["", [Validators.required, Validators.minLength(10)]],
+      Nowa2ZlaOdp: ["", [Validators.required, Validators.minLength(10)]],
+      Nowa3ZlaOdp: ["", [Validators.required, Validators.minLength(10)]],
+    });
   }
-
-
 
   addQuestion() {
     const json: object = {
@@ -43,13 +41,13 @@ export class AddQuestionComponent implements OnInit {
       FakeAnwserThree: this.fakeAnwserThree,
     };
 
-    if (this.AddQuestionForm.invalid)
-    {
-      alert("Każde pole musi być wypełnione! Pytanie musi mieć więcej niż 15 znaków.");
+    if (this.AddQuestionForm.invalid) {
+      alert(
+        "Każde pole musi być wypełnione! Pytanie musi mieć więcej niż 15 znaków."
+      );
     }
 
     console.log(this.AddQuestionForm.value);
     this.qs.postQuestions(json);
-
   }
 }
