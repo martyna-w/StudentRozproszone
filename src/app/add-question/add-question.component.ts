0ddf4601c4005bd3f8b551bba20859d80233de36
martyna-w <59871473+martyna-w@users.noreply.github.com>
2020-06-04 00:08:38 +0200

    00:08

--- src/app/add-question/add-question.component.ts
+++ src/app/add-question/add-question.component.ts
@@ -24,32 +24,32 @@ export class AddQuestionComponent implements OnInit {
 
   ngOnInit() {
     this.AddQuestionForm = this.formBuilder.group({
-      NowePytanie: ['', Validators.required],
-      NowaDobraOdp: ['', Validators.required],
-      Nowa1ZlaOdp: ['', Validators.required],
-      Nowa2ZlaOdp: ['', Validators.required],
-      Nowa3ZlaOdp: ['', Validators.required],
+      NowePytanie: ['', [Validators.required, Validators.minLength(10)]],
+      NowaDobraOdp: ['', [Validators.required, Validators.minLength(10)]],
+      Nowa1ZlaOdp: ['', [Validators.required, Validators.minLength(10)]],
+      Nowa2ZlaOdp: ['', [Validators.required, Validators.minLength(10)]],
+      Nowa3ZlaOdp: ['', [Validators.required, Validators.minLength(10)]],
     })
   }
 
 
 
   addQuestion() {
-    //const json: object = {
-    //  Question: this.description,
-    //  GoodAnwser: this.goodAnwser,
-    //  FakeAnwserOne: this.fakeAnwserOne,
-    //  FakeAnwserTwo: this.fakeAnwserTwo,
-    //  FakeAnwserThree: this.fakeAnwserThree,
-    //};
+    const json: object = {
+      Question: this.description,
+      GoodAnwser: this.goodAnwser,
+      FakeAnwserOne: this.fakeAnwserOne,
+      FakeAnwserTwo: this.fakeAnwserTwo,
+      FakeAnwserThree: this.fakeAnwserThree,
+    };
 
     if (this.AddQuestionForm.invalid)
     {
-      alert("Każde pole musi być wypełnione!");
+      alert("Każde pole musi być wypełnione! Pytanie musi mieć więcej niż 15 znaków.");
     }
 
     console.log(this.AddQuestionForm.value);
-    this.qs.postQuestions(this.AddQuestionForm.value);
+    this.qs.postQuestions(json);
 
   }
 }
